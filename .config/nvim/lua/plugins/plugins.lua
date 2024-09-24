3ab35b0d65a60b26237e8c3784492681961b7584
Aloïs Deniel <alois.deniel@outlook.com>
2024-09-24 13:27:34 +0200

    Update plugins.lua

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -1,9 +1,11 @@
 -- since this is just an example spec, don't actually load anything here and return an empty spec
 return {
+  { "echasnovski/mini.nvim", version = "*" },
   {
     "nvim-treesitter/nvim-treesitter",
     opts = {
       ensure_installed = {
+        "dart",
         "bash",
         "html",
         "json",
@@ -13,6 +15,7 @@ return {
         "regex",
         "templ",
       },
+      indent = { enable = true, disable = { "dart" } },
     },
   },
   {
@@ -33,12 +36,98 @@ return {
     },
   },
   {
-    "akinsho/flutter-tools.nvim",
-    lazy = false,
+    "neovim/nvim-lspconfig",
+    event = "LazyFile",
     dependencies = {
-      "nvim-lua/plenary.nvim",
-      "stevearc/dressing.nvim",
+      "mason.nvim",
+      { "williamboman/mason-lspconfig.nvim", config = function() end },
     },
-    config = true,
+    opts = function()
+      local ret = {
+        diagnostics = {},
+        inlay_hints = {
+          enabled = true,
+        },
+        codelens = {
+          enabled = true,
+        },
+        document_highlight = {
+          enabled = true,
+        },
+        capabilities = {
+          workspace = {
+            fileOperations = {
+              didRename = true,
+              willRename = true,
+            },
+          },
+        },
+        format = {
+          formatting_options = nil,
+          timeout_ms = nil,
+        },
+        servers = {
+          dartls = {
+
+            mason = false,
+            cmd = {
+              "dart",
+              "language-server",
+              "--protocol=lsp",
+            },
+            filetypes = { "dart" },
+            init_options = {
+              onlyAnalyzeProjectsWithOpenFiles = false,
+              suggestFromUnimportedLibraries = true,
+              closingLabels = true,
+              outline = false,
+              flutterOutline = false,
+            },
+            settings = {
+              dart = {
+                analysisExcludedFolders = {
+                  vim.fn.expand("$HOME/AppData/Local/Pub/Cache"),
+                  vim.fn.expand("$HOME/.pub-cache"),
+                  vim.fn.expand("/opt/homebrew/"),
+                  vim.fn.expand("$HOME/tools/flutter/"),
+                },
+                updateImportsOnRename = true,
+                completeFunctionCalls = true,
+                showTodos = true,
+              },
+            },
+          },
+
+          lua_ls = {
+            settings = {
+              Lua = {
+                workspace = {
+                  checkThirdParty = false,
+                },
+                codeLens = {
+                  enable = true,
+                },
+                completion = {
+                  callSnippet = "Replace",
+                },
+                doc = {
+                  privateName = { "^_" },
+                },
+                hint = {
+                  enable = true,
+                  setType = false,
+                  paramType = true,
+                  paramName = "Disable",
+                  semicolon = "Disable",
+                  arrayIndex = "Disable",
+                },
+              },
+            },
+          },
+        },
+        setup = {},
+      }
+      return ret
+    end,
   },
 }
