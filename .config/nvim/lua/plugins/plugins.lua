30ce2cf266b891a7ee6a08d2da881c85cd80c44e
Aloïs Deniel <alois.deniel@outlook.com>
2024-09-27 15:00:57 +0200

    Update plugins.lua

--- .config/nvim/lua/plugins/plugins.lua
+++ .config/nvim/lua/plugins/plugins.lua
@@ -73,16 +73,47 @@ return {
     },
     opts = function()
       local ret = {
-        diagnostics = {},
+        -- options for vim.diagnostic.config()
+        ---@type vim.diagnostic.Opts
+        diagnostics = {
+          underline = true,
+          update_in_insert = false,
+          virtual_text = {
+            spacing = 4,
+            source = "if_many",
+            prefix = "●",
+            -- this will set set the prefix to a function that returns the diagnostics icon based on the severity
+            -- this only works on a recent 0.10.0 build. Will be set to "●" when not supported
+            -- prefix = "icons",
+          },
+          severity_sort = true,
+          signs = {
+            text = {
+              [vim.diagnostic.severity.ERROR] = LazyVim.config.icons.diagnostics.Error,
+              [vim.diagnostic.severity.WARN] = LazyVim.config.icons.diagnostics.Warn,
+              [vim.diagnostic.severity.HINT] = LazyVim.config.icons.diagnostics.Hint,
+              [vim.diagnostic.severity.INFO] = LazyVim.config.icons.diagnostics.Info,
+            },
+          },
+        },
+        -- Enable this to enable the builtin LSP inlay hints on Neovim >= 0.10.0
+        -- Be aware that you also will need to properly configure your LSP server to
+        -- provide the inlay hints.
         inlay_hints = {
           enabled = true,
+          exclude = { "vue" }, -- filetypes for which you don't want to enable inlay hints
         },
+        -- Enable this to enable the builtin LSP code lenses on Neovim >= 0.10.0
+        -- Be aware that you also will need to properly configure your LSP server to
+        -- provide the code lenses.
         codelens = {
-          enabled = true,
+          enabled = false,
         },
+        -- Enable lsp cursor word highlighting
         document_highlight = {
           enabled = true,
         },
+        -- add any global capabilities here
         capabilities = {
           workspace = {
             fileOperations = {
@@ -91,6 +122,9 @@ return {
             },
           },
         },
+        -- options for vim.lsp.buf.format
+        -- `bufnr` and `filter` is handled by the LazyVim formatter,
+        -- but can be also overridden when specified
         format = {
           formatting_options = nil,
           timeout_ms = nil,
@@ -123,33 +157,6 @@ return {
                 updateImportsOnRename = true,
                 completeFunctionCalls = true,
                 showTodos = true,
-              },
-            },
-          },
-
-          lua_ls = {
-            settings = {
-              Lua = {
-                workspace = {
-                  checkThirdParty = false,
-                },
-                codeLens = {
-                  enable = true,
-                },
-                completion = {
-                  callSnippet = "Replace",
-                },
-                doc = {
-                  privateName = { "^_" },
-                },
-                hint = {
-                  enable = true,
-                  setType = false,
-                  paramType = true,
-                  paramName = "Disable",
-                  semicolon = "Disable",
-                  arrayIndex = "Disable",
-                },
               },
             },
           },
