6c7485106563ca03bcbd0046df594d5186686eef
Aloïs Deniel <alois.deniel@outlook.com>
2024-11-18 15:14:05 +0100

    Switched from Wezterm to tmux.

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,40 +2,42 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+local function tmux_command(command)
+  local tmux_socket = vim.fn.split(vim.env.TMUX, ",")[1]
+  return vim.fn.system("tmux -S " .. tmux_socket .. " " .. command)
+end
+
+local function tmux_window_exists(name)
+  local windows = tmux_command("list-windows")
+  local index = string.find(windows, ": " .. name)
+  return index ~= nil and index >= 0
+end
+
 -- Flutter commands
 vim.api.nvim_create_user_command("FlutterRun", function(opts)
-  -- Open a new wezterm pane on the right
-  local command = "wezterm cli split-pane --right --percent 30 --cwd '"
-    .. opts.args
-    .. "' -- bash -c 'flutter run; exec bash'"
-
-  -- Run the command to create the new pane and capture the pane ID
-  local handle = io.popen(command)
-  if handle ~= nil then
-    local result = handle:read("*a")
-    handle:close()
-
-    -- Assuming the pane ID is printed after the pane is created
-    -- Here you would normally extract the pane ID from the command output if necessary.
-    -- In case you need to get pane details use `wezterm cli list --format json`.
-    wezterm_pane_id = result:match("%d+") -- Extract pane ID
+  local window_exists = tmux_window_exists("flutter")
+  if not window_exists then
+    tmux_command("new-window -n flutter -c '" .. opts.args .. "' bash -c 'flutter run; exec bash'")
+    vim.notify("Flutter run started!", vim.log.levels.INFO)
+  else
+    vim.notify("Error: No Tmux window named 'flutter' found. Run :FlutterRun first.", vim.log.levels.ERROR)
   end
 end, { nargs = 1 })
 
 vim.api.nvim_create_user_command("FlutterInput", function(input)
-  if wezterm_pane_id then
-    -- Send input to the stored pane ID
-    local send_command = "wezterm cli send-text --pane-id " .. wezterm_pane_id .. " '" .. input.args .. "'"
-    os.execute(send_command)
+  local window_exists = tmux_window_exists("flutter")
+  if window_exists then
+    local send_command = "send-keys -t flutter '" .. input.args .. "' Enter"
+    tmux_command(send_command)
+    vim.notify("Flutter input '" .. input.args .. "' sent!", vim.log.levels.INFO)
   else
-    print("Error: No WezTerm pane created yet. Run :FlutterRun first.")
+    vim.notify("Error: No Tmux window named 'flutter' found. Run :FlutterRun first.", vim.log.levels.ERROR)
   end
 end, { nargs = 1 })
 
-
 -- Keymaps
 --
-vim.keymap.set("n", "<leader>off", function()
+vim.keymap.set("n", "<leader>rs", function()
   vim.ui.input({ prompt = "In which directory?", default = vim.fn.getcwd() }, function(input)
     if input then
       vim.cmd("FlutterRun " .. input)
@@ -47,19 +49,19 @@ end, {
   silent = true,
 })
 
-vim.api.nvim_set_keymap("n", "<leader>ofr", ":FlutterInput r<CR>", {
+vim.api.nvim_set_keymap("n", "<leader>rr", ":FlutterInput r<CR>", {
   desc = "Hot reload. 🔥🔥🔥",
   noremap = true,
   silent = true,
 })
 
-vim.api.nvim_set_keymap("n", "<leader>ofR", ":FlutterInput R<CR>", {
+vim.api.nvim_set_keymap("n", "<leader>rR", ":FlutterInput R<CR>", {
   desc = "Hot restart.",
   noremap = true,
   silent = true,
 })
 
-vim.api.nvim_set_keymap("n", "<leader>ofq", ":FlutterInput q<CR>", {
+vim.api.nvim_set_keymap("n", "<leader>rq", ":FlutterInput q<CR>", {
   desc = "Quit (terminate the application on the device).",
   noremap = true,
   silent = true,
