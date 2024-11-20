3fae932c091e61c88bf799f28d9362ad8adad846
Aloïs Deniel <alois.deniel@outlook.com>
2024-11-20 16:04:20 +0100

    Update wezterm.lua

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,6 +1,8 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
+config.hide_tab_bar_if_only_one_tab = true
+
 -- Theme
 config.font = wezterm.font("JetBrains Mono")
 config.font_size = 18.0
