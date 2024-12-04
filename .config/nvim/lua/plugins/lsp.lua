dd434a4de22b7dc371685f5d2d92610e996ec5aa
Aloïs Deniel <alois.deniel@gmail.com>
2024-12-04 17:03:00 +0100

    Updated lualine and lsp.

---
+++ .config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,104 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    event = "LazyFile",
+    dependencies = {
+      "mason.nvim",
+      { "williamboman/mason-lspconfig.nvim", config = function() end },
+    },
+    opts = function()
+      local ret = {
+        -- options for vim.diagnostic.config()
+        ---@type vim.diagnostic.Opts
+        diagnostics = {
+          underline = true,
+          update_in_insert = false,
+          virtual_text = {
+            spacing = 4,
+            source = "if_many",
+            prefix = "●",
+            -- this will set set the prefix to a function that returns the diagnostics icon based on the severity
+            -- this only works on a recent 0.10.0 build. Will be set to "●" when not supported
+            -- prefix = "icons",
+          },
+          severity_sort = true,
+          signs = {
+            text = {
+              [vim.diagnostic.severity.ERROR] = LazyVim.config.icons.diagnostics.Error,
+              [vim.diagnostic.severity.WARN] = LazyVim.config.icons.diagnostics.Warn,
+              [vim.diagnostic.severity.HINT] = LazyVim.config.icons.diagnostics.Hint,
+              [vim.diagnostic.severity.INFO] = LazyVim.config.icons.diagnostics.Info,
+            },
+          },
+        },
+        -- Enable this to enable the builtin LSP inlay hints on Neovim >= 0.10.0
+        -- Be aware that you also will need to properly configure your LSP server to
+        -- provide the inlay hints.
+        inlay_hints = {
+          enabled = true,
+          exclude = { "vue" }, -- filetypes for which you don't want to enable inlay hints
+        },
+        -- Enable this to enable the builtin LSP code lenses on Neovim >= 0.10.0
+        -- Be aware that you also will need to properly configure your LSP server to
+        -- provide the code lenses.
+        codelens = {
+          enabled = false,
+        },
+        -- Enable lsp cursor word highlighting
+        document_highlight = {
+          enabled = true,
+        },
+        -- add any global capabilities here
+        capabilities = {
+          workspace = {
+            fileOperations = {
+              didRename = true,
+              willRename = true,
+            },
+          },
+        },
+        -- options for vim.lsp.buf.format
+        -- `bufnr` and `filter` is handled by the LazyVim formatter,
+        -- but can be also overridden when specified
+        format = {
+          formatting_options = nil,
+          timeout_ms = nil,
+        },
+        servers = {
+          dartls = {
+
+            mason = false,
+            cmd = {
+              "dart",
+              "language-server",
+              "--protocol=lsp",
+            },
+            filetypes = { "dart" },
+            init_options = {
+              onlyAnalyzeProjectsWithOpenFiles = false,
+              suggestFromUnimportedLibraries = true,
+              closingLabels = true,
+              outline = false,
+              flutterOutline = false,
+            },
+            settings = {
+              dart = {
+                analysisExcludedFolders = {
+                  vim.fn.expand("$HOME/AppData/Local/Pub/Cache"),
+                  vim.fn.expand("$HOME/.pub-cache"),
+                  vim.fn.expand("/opt/homebrew/"),
+                  vim.fn.expand("$HOME/tools/flutter/"),
+                },
+                updateImportsOnRename = true,
+                completeFunctionCalls = true,
+                showTodos = true,
+              },
+            },
+          },
+        },
+        setup = {},
+      }
+      return ret
+    end,
+  },
+}
