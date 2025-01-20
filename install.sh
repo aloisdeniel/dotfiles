43e5ff5cdcafd8989d8621962cb2fb910c6940b1
Aloïs Deniel <alois.deniel@outlook.com>
2025-01-20 12:39:04 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,20 +12,22 @@ brew upgrade
 # Command line programs
 brew install tmux
 brew install fzf
-brew install ripgrep
-brew install lazygit
-brew install neovim
-brew install ollama
-brew instal yazi
+brew install ripgrep 
+brew install lazygit # Git UI
+brew install neovim # Code editor
+brew install ollama # Local AI models
+brew instal yazi # File manager
 
 #SDKs & Runtimes
 brew install node
 brew install go
 brew install rustup
+brew install --cask corretto # OpenJDK
 brew install --cask flutter
 
 # Programs
-brew install --cask ghostty
+
+brew install --cask ghostty # Terminal
 brew install --cask raycast
 brew install --cask github
 brew install --cask figma
@@ -35,6 +37,3 @@ brew install --cask font-jetbrains-mono-nerd-font
 
 # Remove outdated versions from the cellar.
 brew cleanup
-
-# Go tools
-go install github.com/a-h/templ/cmd/templ@latest
