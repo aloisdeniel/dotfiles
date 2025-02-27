9d11211897f5d54cce5c3e027af92acc81e00bc6
Aloïs DENIEL <alois@click-f07qpypr22.home>
2025-02-27 23:11:05 +0100

    Switched to kickstart.nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,6 +1,143 @@
-require("flutter")
+-- Based on kickstart.nvim
 
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+-- OPTIONS
 
-vim.filetype.add({ extension = { templ = "templ" } })
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+vim.g.have_nerd_font = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.mouse = "a"
+vim.opt.showmode = false
+
+-- Sync clipboard between OS and Neovim.
+vim.schedule(function()
+	vim.opt.clipboard = "unnamedplus"
+end)
+
+vim.opt.breakindent = true
+vim.opt.undofile = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.signcolumn = "yes"
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.opt.list = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+vim.opt.inccommand = "split"
+vim.opt.cursorline = true
+vim.opt.scrolloff = 10
+
+-- KEYMAPS
+
+vim.keymap.set("n", "<leader>qq", "<cmd>qa<cr>", { desc = "Quit All" })
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+vim.keymap.set("n", "<leader>e", "<cmd>Neotree<cr>", { desc = "Open N[e]otree" })
+
+vim.keymap.set("n", "<leader>rs", function()
+	vim.ui.input({ prompt = "In which directory?", default = vim.fn.getcwd() }, function(input)
+		if input then
+			vim.cmd("FlutterRun " .. input)
+		end
+	end)
+end, {
+	desc = "Start the app.",
+	noremap = true,
+	silent = true,
+})
+
+vim.api.nvim_set_keymap("n", "<leader>rr", ":FlutterInput r<CR>", {
+	desc = "Hot reload. 🔥🔥🔥",
+	noremap = true,
+	silent = true,
+})
+
+vim.api.nvim_set_keymap("n", "<leader>rR", ":FlutterInput R<CR>", {
+	desc = "Hot restart.",
+	noremap = true,
+	silent = true,
+})
+
+vim.api.nvim_set_keymap("n", "<leader>rq", ":FlutterInput q<CR>", {
+	desc = "Quit (terminate the application on the device).",
+	noremap = true,
+	silent = true,
+})
+
+-- COMMANDS
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
+
+-- PLUGINS
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		error("Error cloning lazy.nvim:\n" .. out)
+	end
+end ---@diagnostic disable-next-line: undefined-field
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
+	require("plugins.flash"),
+	require("plugins.neotree"),
+	require("plugins.gitsigns"),
+	require("plugins.whichkeys"),
+	require("plugins.telescope"),
+	{
+		"folke/lazydev.nvim",
+		ft = "lua",
+		opts = {
+			library = {
+				{ path = "${3rd}/luv/library", words = { "vim%.uv" } },
+			},
+		},
+	},
+	require("plugins.lspconfig"),
+	require("plugins.cmp"),
+	require("plugins.todo"),
+	require("plugins.mini"),
+	require("plugins.snacks"),
+	require("plugins.treesitter"),
+	require("plugins.tokyonight"),
+	require("plugins.conform"),
+}, {
+	ui = {
+		icons = vim.g.have_nerd_font and {} or {
+			cmd = "⌘",
+			config = "🛠",
+			event = "📅",
+			ft = "📂",
+			init = "⚙",
+			keys = "🗝",
+			plugin = "🔌",
+			runtime = "💻",
+			require = "🌙",
+			source = "📄",
+			start = "🚀",
+			task = "📌",
+			lazy = "💤 ",
+		},
+	},
+})
---
+++ .config/nvim/lua/plugins/neotree.lua
@@ -0,0 +1,31 @@
+-- Neo-tree is a Neovim plugin to browse the file system
+-- https://github.com/nvim-neo-tree/neo-tree.nvim
+
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	version = "*",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	cmd = "Neotree",
+	keys = {
+		{ "\\", ":Neotree reveal<CR>", desc = "NeoTree reveal", silent = true },
+	},
+	opts = {
+		filesystem = {
+			filtered_items = {
+				visible = true,
+			},
+			window = {
+				position = "float",
+				mappings = {
+					["l"] = "open",
+					["h"] = "close_node",
+					["<space>"] = "none",
+				},
+			},
+		},
+	},
+}
